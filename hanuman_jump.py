4da721a4ca0845652ae22daec13f8fc9d999f7ce
PHEAKTRA.MAO <PHEAKTRA.MAO@PNC.LAN>
2023-10-21 17:56:30 +0700

    #1 create image on home page

--- hanuman_jump.py
+++ hanuman_jump.py
@@ -1,10 +1,20 @@
-#________________________IMPORT FILES______________________
+#________________________IMPORT______________________
 import tkinter as tk
 from PIL import ImageTk, Image
-#_________________________CONSTANTS________________________
+#============================ CONSTANTS ============================
+
 WINDOW_WIDTH=1420
 WINDOW_HEIGHT=800
-#_____________________GLOBAL____________
+GRAVITY_FORCE = 9
+JUMP_FORCE = 30
+SPEED = 7
+TIMED_LOOP = 6
+
+#============================ VARIABLES ============================
+
+keyPressed = []
+
+#============================ GLOBAL ============================
 score=0
 
 #________________MAIN WINDOW________________
@@ -17,16 +27,25 @@ canvas=tk.Canvas(frame,width=WINDOW_WIDTH,height=WINDOW_HEIGHT)
 canvas.pack()
 
 #____________IMPORT IMAGES____________
-bg_home=Image.open("IMAGES/bg_home.png")
-bg_size=bg_home.resize((WINDOW_WIDTH,WINDOW_HEIGHT))
-bg=ImageTk.PhotoImage(bg_size)
-canvas.create_image(0,0,image=bg,anchor="nw")
+bg=ImageTk.PhotoImage(file="IMAGES/bg_home.png")
+start=ImageTk.PhotoImage(file="IMAGES/start.png")
+help=ImageTk.PhotoImage(file="IMAGES/help.png")
+exit=ImageTk.PhotoImage(file="IMAGES/exit.png")
+player=ImageTk.PhotoImage(file="IMAGES/player.png")
 
-start_button=Image.open("IMAGES/start_button.png")
-start_button_size=start_button.resize((150,150))
-start=ImageTk.PhotoImage(start_button_size)
-canvas.create_image(600,300,image=start,anchor="nw",tags="start")
-# bg_default=PhotoImage(file="IMAGES/bg_home.png")
 #______________HOME PAGE___________
+def home():
+    canvas.create_image(0,0,image=bg,anchor="nw")
+    canvas.create_image(600,410,image=start,anchor="nw",tags="start")
+    canvas.create_image(600,510,image=help,anchor="nw",tags="help")
+    canvas.create_image(620,610,image=exit,anchor="nw",tags="exit")
+home()
 
+#________________KEY EVENTS_______________
+canvas.tag_bind=("start","<Button-1>",start)
+canvas.tag_bind=("help","<Button-1>",help)
+canvas.tag_bind=("exit","<Button-1>",exit)
+#========================= DISPLAY WINDOW =================
+canvas.pack(expand=True, fill="both")
+frame.pack(expand=True, fill="both")
 window.mainloop()
